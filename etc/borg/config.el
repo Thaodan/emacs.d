e6a5f4c18768df66d73ce23445f2db2142983587
Björn Bidar <bjorn.bidar@thaodan.de>
2024-10-14 07:35:40 +0300

    Borg: Also prefer newer in borg
    Signed-off-by: Björn Bidar <bjorn.bidar@thaodan.de>

--- etc/borg/config.el
+++ etc/borg/config.el
@@ -1,3 +1,7 @@
 
 ;; Override `borg-emacs-arguments' so it doesn't include -Q and thus loads site-lisp
 (setq borg-emacs-arguments '("--batch"))
+
+;; Prefer newer to avoid Emacs loading older byte-compiled files and accidentally
+;; old code to end up in depending files that are about to be byte-compiled.
+(setopt load-prefer-newer t)
