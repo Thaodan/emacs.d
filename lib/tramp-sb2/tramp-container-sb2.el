c872285efc831d888124cf523f40bd8d7caee95e
Björn Kettunen <bjorn.kettunen@thaodan.de>
2025-08-05 09:36:14 +0300

    CHANGE 68 drones
    Update alert                  to v1.3-35-gd17ad05
    Update all-the-icons          to  5.0.0-133-g4778632b2
    Update async                  to v1.9.9-9-gdfbd879
    Update auto-compile           to v2.0.7
    Update autocrypt              to  08c1e2c
    Update backline               to v1.1.0
    Update bitbake-modes          to v0.6.0
    Update borg                   to v4.1.5-4-g5e5b9e2
    Update buttercup              to v1.38
    Update closql                 to v2.2.2
    Update company                to  1.0.2-36-g1924eab
    Update compat                 to  30.1.0.1
    Update crux                   to v0.5.0-10-g3b72275
    Update debbugs                to  22fc3c2
    Update dired-hacks            to  de9336f
    Update dirvish                to  2.3.0-6-gd877433
    Update doom-modeline          to v4.2.0-10-gcb703c2
    Assimilate elisp-autofmt           43a44dc
    Update emacsql                to v4.3.1
    Update ement                  to v0.15.1-48-g57ac200
    Update epkg                   to v4.0.7
    Update fedi                   to  e53f4d6
    Update flycheck               to v35.0-8-ga4d782e7
    Update forge                  to v0.5.3-81-gca6c2ae
    Update ghub                   to v4.3.2-41-ga3a5dd3
    Update git-modes              to v1.4.5
    Update gnus-desktop-notify    to  1.4-35-g344777f
    Update helm-descbinds         to v1.13-21-g0aff44b
    Update helm-ls-git            to v1.9.4-96-g04680c2
    Update helm-projectile        to v1.1.0-5-g886db3c
    Update hl-todo                to v3.8.5
    Update htmlize                to release/1.59-1-gc9a8196
    Update ivy                    to  0.15.1-4-g2257a9d
    Update llama                  to v1.0.0
    Update lsp-mode               to  9.0.0-239-gb96102ce15
    Update lsp-ui                 to  9.0.0-18-gbbb1aa0
    Update magit                  to v4.3.8-113-ge4c186a0
    Update marginalia             to  2.2-1-g3bb3021
    Update markdown-mode          to v2.7-72-gfc4fff8
    Update minions                to v1.1.1
    Update no-littering           to v1.7.9
    Update nov                    to  0.5.0-2-g933816c
    Update org-contacts           to  cbf1f18
    Update org-modern             to  1.9-1-g1723689
    Update org-vcard              to v0.2.0-71-gcca90b1
    Update orgit                  to v2.0.3
    Update orgit-forge            to v1.0.2
    Update outline-minor-faces    to v1.1.4
    Update pass                   to  2.0-34-g7651389
    Update password-store         to  1.7.4-14-g3ca13cd
    Update persp-mode             to v3.0.9-32-gf146ddc
    Update perspective            to  2.20
    Update plantuml-mode          to v1.8.0-3-g348e83f
    Update separedit              to v0.3.37-23-g5cb46a6
    Update smartparens            to  1.11.0-335-gb629b4e
    Update taxy-magit-section     to taxy-magit-section-0.14.3
    Update transient              to v0.9.4
    Update treemacs               to  3.2-14-g5fa8419
    Update use-package            to  2.4.4-15-ga8fbc14
    Update w3m                    to before-drop-emacs28-2025-02-26-9-gbf331574
    Update web-mode               to v17.3.20-11-gf1f22bc
    Update with-editor            to v3.4.5
    Update ws-butler              to v1.3-1-g67c49cf
    Update yasnippet              to  0.14.0-65-gc1e6ff2
    Update yeetube                to  2.1.8-6-gb46fff2

---
+++ lib/tramp-sb2/tramp-container-sb2.el
@@ -0,0 +1,25 @@
+;;; tramp-container.el --- Tramp integration for Docker-like containers  -*- lexical-binding: t; -*-
+
+;; Copyright © 2022-2024 Free Software Foundation, Inc.
+
+;; Author: Brian Cully <bjc@kublai.com>
+;; Maintainer: Michael Albinus <michael.albinus@gmx.de>
+;; Keywords: comm, processes
+;; Package: tramp
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
