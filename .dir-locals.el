4a33b5a992098d94641a44908ccb3ef314e8aea7
Björn Kettunen <bjorn.kettunen@thaodan.de>
2025-11-20 22:58:18 +0200

    Disable the creation of ID's for this file
    We want to create ID's for todos which don't have ID's.
    However not for this file as it isn't related to
    the agenda and links to it should be stable
    without adding ID's, as they user specific.
    
    Mark the new setting of this variable as safe in init.el
    since users of it should of course should assume it as safe
    as it comes from the same place init.el comes from.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-mode . ((magit-todos-exclude-globs . (".git/" "./etc/org/templates/")))))
+((magit-mode . ((magit-todos-exclude-globs . (".git/" "./etc/org/templates/"))))
+ ;; We don't want ID's for the org files of this repository
+ ;; i.e. when linking to this file from another file outside of the repository.
+ (org-mode . ((org-id-link-to-org-use-id . nil))))
