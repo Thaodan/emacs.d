fff542c07aee4eb5536ac0bfe757e1f4e4208ebe
Björn Kettunen <bjorn.kettunen@thaodan.de>
2026-06-11 23:44:17 +0300

    Borg: Rebase patches, update config for recent upstream changes

--- etc/borg/config.mk
+++ etc/borg/config.mk
@@ -1,5 +1,6 @@
-# Override `EMACS_ARGUMENTS' so it doesn't include -Q and thus loads site-lisp
-EMACS_ARGUMENTS := --batch
+# Override `EMACS_Q_ARG' so it doesn't include -Q and thus loads site-lisp
+EMACS_Q_ARG ?= -q
+
 
 # Load borg's init.el also when tangling init.el
 EMACS_EXTRA := -l etc/borg/init.el
